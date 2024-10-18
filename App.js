58b75733abb20befbf8b6a4d6034901b629aa8c8
Anthony Sinchi <anthonysinchi655@gmial.com>
2024-10-18 16:50:35 -0400

    Added my initial files

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up!</Text>
+      <Text>hello</Text>
+      <Text>bey</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
